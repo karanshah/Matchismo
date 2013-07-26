ec8fc547843365bca4df9cde3bf930c39b363861
Karan Shah <shah.karan@gmail.com>
2013-07-25 17:16:07 -0700

    added a tab bar controller and added set game view controller

---
+++ Matchismo/SetViewController.h
@@ -0,0 +1,13 @@
+//
+//  SetViewController.h
+//  Matchismo
+//
+//  Created by Karan Shah on 7/25/13.
+//  Copyright (c) 2013 Karan Shah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SetViewController : UIViewController
+
+@end
---
+++ Matchismo/SetViewController.m
@@ -0,0 +1,38 @@
+//
+//  SetViewController.m
+//  Matchismo
+//
+//  Created by Karan Shah on 7/25/13.
+//  Copyright (c) 2013 Karan Shah. All rights reserved.
+//
+
+#import "SetViewController.h"
+
+@interface SetViewController ()
+
+@end
+
+@implementation SetViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
