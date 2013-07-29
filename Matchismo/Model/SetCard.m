f23094f0e9e93c9105e5b807e64dac91da2d7e53
Karan Shah <shah.karan@gmail.com>
2013-07-28 18:45:25 -0700

    added the logic for matching a set

--- Matchismo/Model/SetCard.m
+++ Matchismo/Model/SetCard.m
@@ -10,9 +10,30 @@
 
 @implementation SetCard
 
-//- (int) match:(NSArray *)otherCards {
-//    
-//}
+- (int) match:(NSArray *)otherCards {
+    int score = 4;
+    
+    NSMutableArray *cards = [[NSMutableArray alloc] initWithArray:otherCards];
+    [cards addObject:self];
+    
+    NSMutableSet *numberSet = [[NSMutableSet alloc] init];
+    NSMutableSet *symbolSet = [[NSMutableSet alloc] init];
+    NSMutableSet *shadingSet = [[NSMutableSet alloc] init];
+    NSMutableSet *colorSet = [[NSMutableSet alloc] init];
+    
+    for (SetCard *card in cards) {
+        [numberSet addObject:[[NSNumber alloc] initWithInt:card.number]];
+        [symbolSet addObject:card.symbol];
+        [shadingSet addObject:card.shading];
+        [colorSet addObject:card.color];
+    }
+    
+    if (numberSet.count == 2 || symbolSet.count == 2 || shadingSet.count == 2 || colorSet.count == 2) {
+        score = 0;
+    }
+    
+    return score;
+}
 
 - (NSString *)contents {
     NSString *content = @"";
